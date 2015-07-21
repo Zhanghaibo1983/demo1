eec33dbef37d15f838ef3c82b0f06d677b31718a
Ron <zhbron@163.com>
2015-07-21 23:20:53 +0800

    hello git

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
 	public void static main(String[] args){
 		System.out.println("hello world");
 		System.out.println("I'm zhanghaibo");
+		System.out.println("I'm studying");
 	}
 }
